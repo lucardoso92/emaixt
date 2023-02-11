51b0cdfb3acdcadf17e98011594b38382c00f093
Lucas Cardoso <dev.lucascardoso@gmail.com>
2023-02-11 01:46:12 -0300

    [fix] regex email

--- emaixt.py
+++ emaixt.py
@@ -19,13 +19,20 @@ class Emaixt:
         return mail
 
     def _get_page(self):
-        headers = {
-            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-        }
-        response = requests.get(url=self.website, headers=headers)
+        try:
+            headers = {
+                'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+            }
+            response = requests.get(
+                url=self.website,
+                headers=headers,
+                timeout=5
+            )
 
-        if response.status_code == 200:
-            return response.text
+            if response.status_code == 200:
+                return response.text
+        except Exception:
+            pass
 
     def _get_emails(self, page: str) -> list:
         page_lxml = html.fromstring(page)
